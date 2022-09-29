f751c0adbdcad45daef0b5a82ec71e3e25302dd3
Nouah <noahgreatakoni@gmail.com>
2022-09-29 13:51:05 +0100

    portfolio added

---
+++ testing.js
@@ -0,0 +1,17 @@
+aboutMe ={
+    "My Story" :  "I am an ever-learning , determined and passion-driven Software Engineer. I have special interest in " +
+        "backend program development using Java and Javascript.Experienced in designing interfaces,developing, testing" +
+        " and debugging codes",
+
+    "Languages" :[ "Java", "JavaScript", "Go","SQL","Html/Css"],
+
+    "DB, Frameworks &  Libraries" : ["MySQL","MongoDb","Docker","Nodejs","React","Springboot"],
+
+    "Other Technologies" :["Unit Testing(Junit,Mockito)","Agile Methodologies","Git","Docker"],
+    "Projects": ["http://github.com"],
+
+    "Softwares": ["Visual Studio  Code","IntelliJ"]
+}
+
+console.log(aboutMe)
+//console.dir(aboutMe)
